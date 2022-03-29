773f4145675b9f429a602d217fdaad850fea8e52
Ivana Mance <ivana.mance@photomath.com>
2022-03-29 13:34:30 +0200

    v0.0.2 with setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pubsub_util",
+    version="0.0.2",
+    author="Ivana Mance",
+    author_email="ivana.mance@photomath.com",
+    description="Util for subscribing or publishing messages to Googles PubSub topics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/photomath/pubsub-util.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)
